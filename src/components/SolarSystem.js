73cdcffd376616663441a71287688fd57a839457
Lucas Lisboa <lucas@pop-os.localdomain>
2022-01-25 16:45:12 -0300

    requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ image, name }) => (<PlanetCard
+          key={ name }
+          planetImage={ image }
+          planetName={ name }
+        />))}
       </div>
     );
   }
